9cc1ba26e8b9fdce37169a6071f31054d302ac38
yashwanthrao425@gmail.com <yashwanthrao425@gmail.com>
2024-05-12 13:06:21 +0530

    added browser Parameter in Base test class and updated pom

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -7,33 +7,42 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import Utilities.readtestData;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Step;
 
 public class BaseTest {
-	
+
 	public WebDriver driver;
-	
+
 	@BeforeMethod
+	@Parameters("BrowserName")
 	@Step("Start the application")
-	public void setup() throws InterruptedException, IOException {
+	public void setup(String browserName) throws InterruptedException, IOException {
+
+		System.out.println("Parameter value is " + browserName);
 		
-		
-		
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();		
-		
+		WebDriver driver = null;
+		if (browserName.contains("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browserName.contains("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+
 		driver.get(readtestData.getURL());
 		driver.manage().window().maximize();
 		Thread.sleep(2000);
 	}
-	
+
 	@AfterMethod
 	@Step("Stop the application")
 	public void tearDown() {
